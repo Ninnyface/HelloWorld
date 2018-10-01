c890230a92c478bb1482542093fbe1562e589d41
b5005129 <b5005129@17DCOMPD138>
2018-10-01 14:09:17 +0100

    Add project files.

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello" << ' ' << "World" << "\n";
+	std::cout << "What is 7 + 2?" << "\n";
+	int xasdsas;
+	cin >> xasdsas;
+
+	return 0;
+}
