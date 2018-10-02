7104219d9e8d570903e56341dc2d8343163fad47
Joe <j.bridgman1@newcastle.ac.uk>
2018-10-02 13:33:40 +0100

    yes

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -4,10 +4,21 @@ using namespace std;
 
 int main() {
 
+	bool isTrue = true;
+	char a = 'a';
+	double d = 4.12421;
+
 	cout << "Hello" << ' ' << "World" << "\n";
-	std::cout << "What is 7 + 2?" << "\n";
-	int xasdsas;
-	cin >> xasdsas;
+	cout << "bool: " << isTrue << "\n";
+	cout << "char: " << a << "\n";
+	cout << "double: " << d << "\n";
+	cout << "Enter the first number." << "\n";
+	int x;
+	cin >> x;
+	cout << "Enter the second number." << "\n";
+	int y;
+	cin >> y;
+
 
 	return 0;
 }
